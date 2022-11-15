9eea9988cadb8b15f336259c87753fe21d6c6960
mingle1989 <minglexmacabre@gmail.com>
2022-11-15 16:33:43 -0500

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
+// Background circles
 import BackgroundCircles from '../BackgroundCircles/BackgroundCircles';
 // import {motion} from 'framer-motion';
+// React simple typewriter
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
+// images
+import images from '../../assets/images/index';
 
 const Hero = () => {
 	const [text, count] = useTypewriter({
@@ -13,8 +17,13 @@ const Hero = () => {
 		delaySpeed: 2000,
 	});
 	return (
-		<div>
+		<div className="text-center flex justify-center items-center flex-col h-screen">
 			<BackgroundCircles />
+			<img
+				className="relative w-[200px] h-[176px] mx-auto object-cover"
+				src={images.Server}
+				alt="server"
+			/>
 			<h1>
 				<span>{text}</span>
 				<Cursor cursorColor="#08b57b" />
